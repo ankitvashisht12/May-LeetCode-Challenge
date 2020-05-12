8d837b0b2d1762e1788160ce8cdc7ebe03b1138f
Ankit Vashisht <ankitvashisht12@gmail.com>
2020-05-12 22:46:01 +0530

    Update Day 12

--- Day-12.cpp
+++ Day-12.cpp
@@ -1,15 +1,48 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int ans = 0, i=0;
-        int n = nums.size();
-        if(n == 1) return nums[i];
 
-        for(; i<n-1; i+=2)
-            ans ^= nums[i]^nums[i+1];
+//         log(n) solution
         
-        ans ^= nums[n-1];
+        int low = 0, high = nums.size()-1;
+            
+        if(high == 0) return nums[high];
         
-        return ans;
+        while(low < high){
+            
+            int mid = low + ((high- low) / 2);
+            
+            if(nums[mid] != nums[mid-1] and nums[mid] != nums[mid+1]){
+                return nums[mid];
+            }
+            else if(mid % 2 == 0){
+                if(nums[mid+1] == nums[mid]){
+                    low = mid+2;
+                }else high = mid - 2;
+            }
+            else{
+                if(nums[mid+1] == nums[mid]){
+                    high = mid - 1;
+                }else low = mid + 1;
+            }
+        }
+        
+        return nums[low];
+        
+        
+//         O(n) Solution
+
+//         int ans = 0, i=0;
+//         int n = nums.size();
+//         if(n == 1) return nums[i];
+
+//         for(; i<n-1; i+=2)
+//             ans ^= nums[i]^nums[i+1];
+        
+//         ans ^= nums[n-1];
+        
+//         return ans;
+        
+        
     }
 };
